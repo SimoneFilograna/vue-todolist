1f9ab7c8e3e9b0974aa8c46cb5bb304c3a9ea3b8
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-21 15:56:16 +0200

    array-created

--- js/main.js
+++ js/main.js
@@ -5,7 +5,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            todoList:[
+                {
+                    id: 0,
+                    text: "Comprare arachidi affettate da un pipistrello AUSTRALIANO",
+                    done: false,       
+                }, 
+                {
+                    id: 1,
+                    text: "Gaurdare Netflix per trovare errori",
+                    done: false,       
+                }, 
+                {
+                    id: 2,
+                    text: "Creare una mail di protesta contro la casa produttrice",
+                    done: false,       
+                }, 
+                {
+                    id: 3,
+                    text: "Bere una coca-zero per festeggiare quanto fatto",
+                    done: false,       
+                } 
+            ]
         }
     }
 }).mount('#app')
